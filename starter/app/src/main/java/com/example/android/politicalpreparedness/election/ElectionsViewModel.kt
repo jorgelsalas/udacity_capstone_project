4e2890670a624dd13d3a8d7ea4cf5a918e7e579a
jorgelsalas <jorgesalch@yahoo.com>
2021-05-09 01:33:26 -0600

    Fixes data types and creates method to trigger navigation.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -10,17 +10,19 @@ import com.example.android.politicalpreparedness.network.models.Election
 class ElectionsViewModel(private val dataSource: ElectionDao): ViewModel() {
 
     //TODO: Create live data val for upcoming elections
-    private val _upcomingElections = MutableLiveData<Election>()
-    val upcomingElections: LiveData<Election>
+    private val _upcomingElections = MutableLiveData<List<Election>>()
+    val upcomingElections: LiveData<List<Election>>
         get() = _upcomingElections
 
     //TODO: Create live data val for saved elections
-    private val _savedElections = MutableLiveData<Election>()
-    val savedElections: LiveData<Election>
+    private val _savedElections = MutableLiveData<List<Election>>()
+    val savedElections: LiveData<List<Election>>
         get() = _savedElections
 
     //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
 
     //TODO: Create functions to navigate to saved or upcoming election voter info
+    fun onElectionClicked(election: Election) {
 
+    }
 }
