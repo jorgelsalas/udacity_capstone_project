b5743df4a2ace57ed77dfd22a906ec97791c4302
jorgelsalas <jorgesalch@yahoo.com>
2021-05-09 01:10:31 -0600

    Adds live data fields for upcoming and saved elections.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.models.Election
 
 //TODO: Construct ViewModel and provide election datasource
 class ElectionsViewModel(private val dataSource: ElectionDao): ViewModel() {
 
     //TODO: Create live data val for upcoming elections
+    private val _upcomingElections = MutableLiveData<Election>()
+    val upcomingElections: LiveData<Election>
+        get() = _upcomingElections
 
     //TODO: Create live data val for saved elections
+    private val _savedElections = MutableLiveData<Election>()
+    val savedElections: LiveData<Election>
+        get() = _savedElections
 
     //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
 
