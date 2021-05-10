e0b1c529179bef08efa26ebf8c7aa8c8bcc20b28
jorgelsalas <jorgesalch@yahoo.com>
2021-05-10 02:34:49 -0600

    Implements fetching voter info and updating the UI.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,16 +1,23 @@
 package com.example.android.politicalpreparedness.election
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.view.View.INVISIBLE
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.politicalpreparedness.database.ElectionDatabase
+import androidx.navigation.fragment.navArgs
 import com.example.android.politicalpreparedness.database.ElectionDatabase.Companion.getInstance
 import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+import com.example.android.politicalpreparedness.network.models.State
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 
 class VoterInfoFragment : Fragment() {
 
     private lateinit var voterInfoViewModel: VoterInfoViewModel
+
+    private lateinit var binding : FragmentVoterInfoBinding
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -20,7 +27,7 @@ class VoterInfoFragment : Fragment() {
         initViewModel()
 
         //TODO: Add binding values
-        val binding = FragmentVoterInfoBinding.inflate(inflater)
+        binding = FragmentVoterInfoBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = voterInfoViewModel
 
@@ -28,7 +35,11 @@ class VoterInfoFragment : Fragment() {
         /**
         Hint: You will need to ensure proper data is provided from previous fragment.
         */
-
+        val args: VoterInfoFragmentArgs by navArgs()
+        voterInfoViewModel.fetchVoterInfo(args.argElectionId.toLong(), args.argDivision)
+        voterInfoViewModel.voterInfo.observe(viewLifecycleOwner, Observer {
+            updateVoterInfoViews(it)
+        })
 
         //TODO: Handle loading of URLs
 
@@ -40,6 +51,40 @@ class VoterInfoFragment : Fragment() {
     private fun initViewModel() {
         val viewModelFactory = VoterInfoViewModelFactory(getInstance(requireContext().applicationContext).electionDao)
         voterInfoViewModel = ViewModelProvider(this, viewModelFactory).get(VoterInfoViewModel::class.java)
+    }
+
+    private fun updateVoterInfoViews(voterInfoResponse: VoterInfoResponse) {
+        if (voterInfoResponse.state == null || voterInfoResponse.state.isEmpty()) {
+            hideAllStateInfoViews()
+        }
+        else {
+            hideViewsBasedOnDataAvailability(voterInfoResponse.state[0])
+        }
+    }
+
+    private fun hideAllStateInfoViews() {
+        with(binding) {
+            stateHeader.visibility = INVISIBLE
+            stateLocations.visibility = INVISIBLE
+            stateBallot.visibility = INVISIBLE
+            addressGroup.visibility = INVISIBLE
+        }
+    }
+
+    private fun hideViewsBasedOnDataAvailability(state: State) {
+        with(state) {
+            if (electionAdministrationBody.ballotInfoUrl == null) {
+                binding.stateBallot.visibility = INVISIBLE
+            }
+
+            if (electionAdministrationBody.votingLocationFinderUrl == null) {
+                binding.stateLocations.visibility = INVISIBLE
+            }
+
+            if (electionAdministrationBody.correspondenceAddress == null) {
+                binding.addressGroup.visibility = INVISIBLE
+            }
+        }
     }
 
     //TODO: Create method to load URL intents
--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,13 +1,28 @@
 package com.example.android.politicalpreparedness.election
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.Division
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
 
     //TODO: Add live data to hold voter info
+    private val _voterInfo = MutableLiveData<VoterInfoResponse>()
+    val voterInfo: LiveData<VoterInfoResponse>
+        get() = _voterInfo
 
     //TODO: Add var and methods to populate voter info
+    private val _loadingVoterInfo = MutableLiveData<Boolean>()
+    val loadingVoterInfo: LiveData<Boolean>
+        get() = _loadingVoterInfo
 
     //TODO: Add var and methods to support loading URLs
 
@@ -17,5 +32,22 @@ class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
     /**
      * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
      */
+
+    fun fetchVoterInfo(electionId: Long, division: Division) {
+        viewModelScope.launch {
+            try {
+                _loadingVoterInfo.value = true
+                _voterInfo.value = CivicsApi.retrofitService.getVoterInfo(getAddress(division), electionId)
+                _loadingVoterInfo.value = false
+            }
+            catch (e: Exception) {
+                Log.e(VoterInfoViewModel::class.java.simpleName, "Unable to fetch voter info: $e")
+            }
+        }
+    }
+
+    private fun getAddress(division: Division) : String {
+        return "${division.state}, ${division.country}"
+    }
 
 }
