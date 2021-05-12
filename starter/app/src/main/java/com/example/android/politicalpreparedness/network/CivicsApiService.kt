fdc56c9c0988ce8c9db3a36108383d8dd90b8f6c
jorgelsalas <jorgesalch@yahoo.com>
2021-05-12 03:18:36 -0600

    Adds the proper return type to the getRepresentatives api method.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.politicalpreparedness.network
 
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
 import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
@@ -41,10 +42,8 @@ interface CivicsApiService {
     suspend fun getVoterInfo(@Query("address") address: String,
                              @Query("electionId") electionId: Long) : VoterInfoResponse
 
-
-    //TODO: Add representatives API Call
     @GET("representatives")
-    suspend fun getRepresentatives(@Query("address") address: String)
+    suspend fun getRepresentatives(@Query("address") address: String) : RepresentativeResponse
 }
 
 object CivicsApi {
