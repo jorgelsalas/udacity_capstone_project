0cf8a97501b82a203ad89453a0437ab9a178b7a2
jorgelsalas <jorgesalch@yahoo.com>
2021-05-13 03:33:14 -0600

    Adds an initial implementation of the getAddressFromFields function.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -22,14 +22,12 @@ class RepresentativeViewModel: ViewModel() {
     val loadingRepresentatives: LiveData<Boolean>
         get() = _loadingRepresentatives
 
-    //TODO: Create function to fetch representatives from API from a provided address
     fun fetchRepresentatives(address: Address) {
         viewModelScope.launch {
             _loadingRepresentatives.value = true
 
             try {
-                // TODO: Use more specific address
-                val (offices, officials) = CivicsApi.retrofitService.getRepresentatives("${address.state}, ${address.zip}")
+                val (offices, officials) = CivicsApi.retrofitService.getRepresentatives(getAddressFromFields(address))
                 _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
             }
             catch (e: Exception) {
@@ -40,19 +38,13 @@ class RepresentativeViewModel: ViewModel() {
         }
     }
 
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
-
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
-
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
-
     //TODO: Create function get address from geo location
 
     //TODO: Create function to get address from individual fields
-
+    private fun getAddressFromFields(address: Address): String {
+        with(address) {
+            //return "$line1, $line2, $city, $state, $zip"
+            return "$state, $zip"
+        }
+    }
 }
